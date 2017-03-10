464c47693bd6e41ffd988296edf6b5074cacb73c
zhengyi <zhengyi@qq.com>
2017-03-10 10:21:40 +0800

    10:21

--- resources/views/mobile/news_details.blade.php
+++ resources/views/mobile/news_details.blade.php
@@ -3,5 +3,15 @@
 	<div class='news_banner'>
 		<img src="{{asset('/mobile/img/test13.png')}}">
 	</div>
+	<div class='news_content'>
+		<div class='title'>
+			<p>8个轮子的「自动小汽车」，你敢来坐吗？—LI创意报告vol.8</p>
+			<span>时间：2017-02-16 14:42:40</span>
+		</div>
+		<div class='content'>
+			
+		</div>
+	</div>
 </div>
+<div class='returnTop'></div>
 @include('mobile.footer')
