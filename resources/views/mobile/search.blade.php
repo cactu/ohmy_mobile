013e392f589e06f6bc85e3a867f28d99a7ffc221
zhengyi <zhengyi@qq.com>
2017-03-11 12:07:48 +0800

    12:07

--- resources/views/mobile/search.blade.php
+++ resources/views/mobile/search.blade.php
@@ -1,3 +1,71 @@
 @include('mobile.header')
-
+<div class='wrap1 searchDiv'>
+	<p class='search_title'>关于“<span>111</span>”的搜索结果</p>
+	<!-- <div class='cards_list'>
+		<ul>
+			<li class='clearfix'>
+				<a href="">
+					<div class='img_holder'>
+						<img src="{{asset('/mobile/img/test.jpg')}}">
+						<span class='num'>1111</span>
+						<span class='recommend'>
+							<img src="{{asset('/mobile/img/recommend.png')}}">
+						</span>
+					</div>
+					<div class='title'>全家人同骑的休闲自行车对的自行车</div>
+					<div class='name'>洋葱zz&nbsp;&nbsp;10岁</div>
+					<div class='involved clearfix'> -->
+						<!-- <img src="{{asset('/mobile/img/nobody.png')}}">
+						<span class='none'>等待热心设计师加入</span> -->
+							<!-- <ul class='clearfix'>
+								<li class='inven'>创意实现:</li>
+								<li class='clearfix'>
+									<img src="{{asset('/mobile/img/test9.png')}}">
+								</li>
+								<li class="more">•••</li>
+							</ul>
+												</div>
+											</a>
+										</li>
+									</ul>
+								</div>
+								<div class='more_inventions'>
+									<span>查看更多</span>
+								</div> -->
+	<p class='search_none'>哎呀~没有找到你想要的发明~可以看看其他发明哟</p>
+	<div class='painting_more'>
+		<p>更多发明</p>
+		<div class='cards_list'>
+			<ul>
+				<li class='clearfix'>
+					<a href="">
+						<div class='img_holder'>
+							<img src="{{asset('/mobile/img/test.jpg')}}">
+							<span class='num'>1111</span>
+							<span class='recommend'>
+								<img src="{{asset('/mobile/img/recommend.png')}}">
+							</span>
+						</div>
+						<div class='title'>全家人同骑的休闲自行车对的自行车</div>
+						<div class='name'>洋葱zz&nbsp;&nbsp;10岁</div>
+						<div class='involved clearfix'>
+							<!-- <img src="{{asset('/mobile/img/nobody.png')}}">
+							<span class='none'>等待热心设计师加入</span> -->
+								<ul class='clearfix'>
+									<li class='inven'>创意实现:</li>
+									<li class='clearfix'>
+										<img src="{{asset('/mobile/img/test9.png')}}">
+									</li>
+									<li class="more">•••</li>
+								</ul>
+						</div>
+					</a>
+				</li>
+			</ul>
+		</div>
+		<div class='more_inventions'>
+			<span>查看更多</span>
+		</div>
+	</div>
+</div>
 @include('mobile.footer')
