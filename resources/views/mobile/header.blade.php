1cf884a8c24f10260840846c914965af4ef36fba
zhengyi <zhengyi@qq.com>
2017-03-13 11:08:00 +0800

    11:07

--- resources/views/mobile/header.blade.php
+++ resources/views/mobile/header.blade.php
@@ -13,10 +13,11 @@
 <head lang="en">
     <meta charset="UTF-8">
     <title>{{$webTitle}}</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
 	<meta name="Keywords" content="Little Inventors,小小发明家,LI小小发明家,发明,儿童创意,儿童教育,脑洞,梦想实现,设计,设计师,创意作品展示,创意灵感" />
 	<meta name="Description" content="Little Inventors小小发明家是由英国发明家Dominic Wilcox与OHMY Design共同发起的全球性儿童创意活动。我们将在全国各地征集4-14岁小朋友的创意发明，并与专业设计师合作，把小朋友的发明实现。我们将打造中国最完善的儿童创意教育体系，和孩子们一起，把世界变成我们想象的样子！" />
 	<meta name='Googlebot' content='index, nofollow, archive'>
-    <meta name="viewport" content="initial-scale=1,maximum-scale=1, minimum-scale=1">
+    
 	<link rel="icon" href="{{asset('/dream/img/llogo.ico')}}" type="image/x-icon"/>
     <link rel="stylesheet" href="//cdn.bootcss.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <link rel="stylesheet" href="//cdn.bootcss.com/normalize/4.2.0/normalize.min.css">
@@ -25,10 +26,10 @@
     <link rel="stylesheet" href="{{asset('/mobile/css/common.css')}}"> 	
 	    
     <script type="text/javascript" src="//cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
-    <script>
-    $(document).ready(function(){
+    <script type="text/javascript">
+    (function resizeFont(){
         document.documentElement.style.fontSize=document.documentElement.clientWidth/7.5+'px';
-    })
+    })()
     </script>
 </head>
 <body class='bodybg'>
